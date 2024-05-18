d2a400b2e142f53165c95e708c13eccf37e88941
pengdong <2019262928@qq.com>
2024-05-18 14:53:18 +0800

    Revert "Merge branch 'main' of github.com:MDXueKui/2037-26FD"
    This reverts commit 2e93c314a68a2a9d62870d494385cc33be992022, reversing
    changes made to 2deda056069464bc6ba4cbb49202c56c858ba215.

--- Source/main.c
+++ Source/main.c
@@ -1,13 +1,17 @@
 /* Includes */
 #include "main.h"
 #include "uart_debug.h"
+#include "tm1650.h"
+#include "iwdg.h"
+#include "input_scan.h"
 
 /* FreeRTOS includes */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "list.h"
-#include "malloc.h"
+#include "timers.h"
+#include "event_groups.h"
 
 /******************************************************************************************************/
 /*FreeRTOS配置*/
@@ -22,21 +26,22 @@ TaskHandle_t            StartTask_Handler;  /* 任务句柄 */
 /* Dispaly_Task 任务 配置
  * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
  */
-#define TASK1_PRIO      2                   /* 任务优先级 */
-#define TASK1_STK_SIZE  128                 /* 任务堆栈大小 */
-TaskHandle_t            Task1Task_Handler;  /* 任务句柄 */
+#define Dispaly_Task_PRIO   2                   /* 任务优先级 */
+#define Dispaly_Task_SIZE   256                 /* 任务堆栈大小 */
+TaskHandle_t    Dispaly_Task_Handler;  /* 任务句柄 */
 
 /* Input_Scan_Task 任务 配置
  * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
  */
-#define TASK2_PRIO      3                   /* 任务优先级 */
-#define TASK2_STK_SIZE  128                 /* 任务堆栈大小 */
-TaskHandle_t            Task2Task_Handler;  /* 任务句柄 */
+#define Input_Scan_Task_PRIO    3                   /* 任务优先级 */
+#define Input_Scan_Task_SIZE    256                 /* 任务堆栈大小 */
+TaskHandle_t    Input_Scan_Task_Handler;  /* 任务句柄 */
 
-List_t                  TestList;           /* 定义测试列表 */
-ListItem_t              ListItem1;          /* 定义测试列表项1 */
-ListItem_t              ListItem2;          /* 定义测试列表项2 */
-ListItem_t              ListItem3;          /* 定义测试列表项3 */
+EventGroupHandle_t EventGroupHandler;       /* 事件标志组句柄 */
+
+#define EVENTBIT_0      (1 << 0)            /* 事件位 */
+#define EVENTBIT_1      (1 << 1)
+#define EVENTBIT_ALL    (EVENTBIT_0 | EVENTBIT_1)
 
 /* User create task */
 static void start_task(void *pvParameters); /* 任务函数声明 */
@@ -44,9 +49,9 @@ static void start_task(void *pvParameters); /* 任务函数声明 */
 int main(void)
 {
     uart_debug_init();
-    APM_MINI_LEDInit(LED2);
-    my_mem_init(SRAMIN);
-
+    input_scan_init();
+    TM1650_init();
+//    IWDTInit();
     xTaskCreate((TaskFunction_t )start_task,            /* 任务函数 */
                 (const char*    )"start_task",          /* 任务名称 */
                 (uint16_t       )START_STK_SIZE,        /* 任务堆栈大小 */
@@ -68,6 +73,8 @@ int main(void)
 void start_task(void *pvParameters)
 {
     taskENTER_CRITICAL();           /* 进入临界区 */
+    /* 创建事件标志组 */
+    EventGroupHandler = xEventGroupCreate();
     /* 创建任务1 */
     xTaskCreate((TaskFunction_t )Dispaly_Task,                 /* 任务函数 */
                 (const char*    )"Dispaly_Task",               /* 任务名称 */
@@ -81,8 +88,8 @@ void start_task(void *pvParameters)
                 (const char*    )"Input_Scan_Task",               /* 任务名称 */
                 (uint16_t       )Input_Scan_Task_SIZE,        /* 任务堆栈大小 */
                 (void*          )NULL,                  /* 传入给任务函数的参数 */
-                (UBaseType_t    )TASK2_PRIO,            /* 任务优先级 */
-                (TaskHandle_t*  )&Task2Task_Handler);   /* 任务句柄 */
+                (UBaseType_t    )Input_Scan_Task_PRIO,            /* 任务优先级 */
+                (TaskHandle_t*  )&Input_Scan_Task_Handler);   /* 任务句柄 */
 
     vTaskDelete(StartTask_Handler); /* 删除开始任务 */
     taskEXIT_CRITICAL();            /* 退出临界区 */
